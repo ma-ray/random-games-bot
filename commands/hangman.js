cb400b18f24a8dcd6dfd35531bf6642981321613
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-01-21 23:56:43 -0500

    Made it so the bot edits the hangman game instead of sending a new one

--- commands/hangman.js
+++ commands/hangman.js
@@ -11,11 +11,12 @@ const imageArray = ['https://i.imgur.com/4fUw8pk.png',
 module.exports = {
 	name: 'hangman',
 	description: 'Play Hangman',
-	execute(message) {
+	async execute(message) {
 
 		const secretWord = words.list[Math.floor(Math.random() * words.list.length)];
 		const displayWord = Array(secretWord.length).fill('█');
 		let attempts = 6;
+		let guess = false;
 
 		const filter = m => m.author.id === message.author.id;
 		const collector = message.channel.createMessageCollector(filter, { max: 26, time: 300000 });
@@ -28,7 +29,7 @@ module.exports = {
 			.setImage(imageArray[attempts]);
 
 		console.log('Initialized Hangman');
-		message.channel.send(embed);
+		let msg = await message.channel.send(embed);
 
 		collector.on('collect', m => {
 			console.log(`User recorded ${m.content}`);
@@ -44,41 +45,53 @@ module.exports = {
 							displayWord[i] = m.content;
 						}
 					}
+
+					embed = new Discord.MessageEmbed()
+						.setColor('#0099ff')
+						.addField('Attempts: ' + attempts, outputWord(displayWord), true)
+						.setTimestamp()
+						.setImage(imageArray[attempts]);
+					msg.edit(embed);
 				} else {
 					attempts--;
-					if (attempts < 1) {
-						embed = new Discord.MessageEmbed()
-							.setColor('#FF0000')
-							.setTitle('Hangman')
-							.addField('You lose!', secretWord, true)
-							.setTimestamp()
-							.setImage(imageArray[attempts]);
-
-						message.channel.send(embed);
-						collector.stop();
-						return;
-					}
+					embed = new Discord.MessageEmbed()
+						.setColor('#0099ff')
+						.addField('Attempts: ' + attempts, outputWord(displayWord), true)
+						.setTimestamp()
+						.setImage(imageArray[attempts]);
+					msg.edit(embed);
 				}
+				
+				
+			}
 
+			if (attempts < 1) {
 				embed = new Discord.MessageEmbed()
-					.setColor('#0099ff')
-					.setTitle('Hangman')
-					.addField('Attempts: ' + attempts, outputWord(displayWord), true)
+					.setColor('#FF0000')
+					.addField('You lose!', secretWord, true)
 					.setTimestamp()
 					.setImage(imageArray[attempts]);
-				message.channel.send(embed);
 
-				if (!displayWord.includes('█')) {
-					message.channel.send('You win!');
-					console.log('Game over');
-					collector.stop();
-				}
+				msg.edit(embed)
+				collector.stop();
+				return;
+			}
+
+			if (!displayWord.includes('█')) {
+				embed = new Discord.MessageEmbed()
+					.setColor()
+					.addField('You win!', secretWord, true)
+					.setTimestamp();
+				msg.edit(embed);
+				message.channel.send('You win!');
+				console.log('Game over');
+				collector.stop();
 			}
 		});
 
 		collector.on('end', collected => {
 			console.log(`Collected ${collected.size} items`);
-			message.channel.send('Game over');
+			message.channel.send('Game over!');
 		});
 	},
 };
