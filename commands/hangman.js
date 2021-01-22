5526efa59cd49aec0a719352a1729bcc827518fb
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-01-22 00:28:54 -0500

    Rewrote the hangman game logic. Now support guessing the word or by char

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
 const words = require('./words.json');
+
+const RED = '#FF0000';
+const BLUE = '#0093FF';
+const GREEN = '#2ECC71';
+
 const imageArray = ['https://i.imgur.com/4fUw8pk.png',
 					'https://i.imgur.com/JSItDs4.png',
 					'https://i.imgur.com/yIH3jOU.png',
@@ -12,19 +17,22 @@ module.exports = {
 	name: 'hangman',
 	description: 'Play Hangman',
 	async execute(message) {
-
 		const secretWord = words.list[Math.floor(Math.random() * words.list.length)];
-		const displayWord = Array(secretWord.length).fill('█');
-		let attempts = 6;
-		let guess = false;
+		let displayWord = Array(secretWord.length).fill('█');
 
 		const filter = m => m.author.id === message.author.id;
 		const collector = message.channel.createMessageCollector(filter, { max: 26, time: 300000 });
 
+		// Variables for the embed
+		let color = GREEN;
+		let attempts = 6;
+		let title = 'Hangman';
+		let field = 'Attempts: ' + attempts;
+
 		let embed = new Discord.MessageEmbed()
-			.setColor('#2ECC71')
-			.setTitle('Hangman')
-			.addField('Attempts: ' + attempts, outputWord(displayWord), true)
+			.setColor(color)
+			.setTitle(title)
+			.addField(field, outputWord(displayWord), true)
 			.setTimestamp()
 			.setImage(imageArray[attempts]);
 
@@ -36,57 +44,58 @@ module.exports = {
 
 			// Delete the user's input message
 			m.delete();
+
 			// Single char mode
 			if (m.content.length === 1) {
-
 				if (secretWord.includes(m.content)) {
 					for (let i = 0; i < secretWord.length; i++) {
 						if (m.content === secretWord[i]) {
 							displayWord[i] = m.content;
 						}
-					}
+					} 
+					color = BLUE;
+					field = 'Attempts: ' + attempts;
 
-					embed = new Discord.MessageEmbed()
-						.setColor('#0099ff')
-						.addField('Attempts: ' + attempts, outputWord(displayWord), true)
-						.setTimestamp()
-						.setImage(imageArray[attempts]);
-					msg.edit(embed);
+					// Win condition
+					if (!displayWord.includes('█')) {
+						color = GREEN;
+						displayWord = secretWord.split('');
+						field = 'You win!';
+						collector.stop();
+					}
 				} else {
 					attempts--;
-					embed = new Discord.MessageEmbed()
-						.setColor('#0099ff')
-						.addField('Attempts: ' + attempts, outputWord(displayWord), true)
-						.setTimestamp()
-						.setImage(imageArray[attempts]);
-					msg.edit(embed);
+					field = 'Attempts: ' + attempts;
 				}
-				
-				
+			} else {
+				// Win condition
+				if (m.content === secretWord) {
+					color = GREEN;
+					displayWord = secretWord.split('');
+					field = 'You win!';
+					collector.stop();
+				} else {
+					attempts--;
+					field = 'Attempts: ' + attempts;
+				}
 			}
 
+			// Loss condition
 			if (attempts < 1) {
-				embed = new Discord.MessageEmbed()
-					.setColor('#FF0000')
-					.addField('You lose!', secretWord, true)
-					.setTimestamp()
-					.setImage(imageArray[attempts]);
-
-				msg.edit(embed)
-				collector.stop();
-				return;
-			}
-
-			if (!displayWord.includes('█')) {
-				embed = new Discord.MessageEmbed()
-					.setColor()
-					.addField('You win!', secretWord, true)
-					.setTimestamp();
-				msg.edit(embed);
-				message.channel.send('You win!');
-				console.log('Game over');
+				color = RED;
+				displayWord = secretWord.split('');
+				field = 'You lose!';
 				collector.stop();
 			}
+
+			embed = new Discord.MessageEmbed()
+				.setColor(color)
+				.setTitle(title)
+				.addField(field, outputWord(displayWord), true)
+				.setTimestamp()
+				.setImage(imageArray[attempts]);
+
+			msg.edit(embed);
 		});
 
 		collector.on('end', collected => {
