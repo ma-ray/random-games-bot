d605fd0a7eeadde5c4305908abdba14235942f91
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-01-25 00:35:41 -0500

    basic funcitonality of blackjack implemented!

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -18,7 +18,38 @@ module.exports = {
 
         console.log('Initialized Blackjack');
 
-        let msg = await message.channel.send('Test');
+        deck.shuffleDeck();
+
+        // Draw cards for the dealer
+        let dealerHand = [deck.draw(), deck.draw()];
+        console.log('Dealer hand');
+        console.log(dealerHand[0]);
+        console.log(dealerHand[1]);
+
+        //Draw cards for the player
+        let playerHand = [deck.draw(), deck.draw()];
+        console.log('Player hand');
+        console.log(playerHand[0]);
+        console.log(playerHand[1]);
+
+        // Variables for the embed
+		// let color = GREEN;
+		let attempts = 6;
+		let title = 'Blackjack';
+        let field = 'Attempts: ' + attempts;
+        let playerContent = 'The total is ' + sumHand(playerHand);
+        let dealerContent = `${dealerHand[0].num} of ${dealerHand[1].suite}\n *One Face Down*`;
+
+		let embed = new Discord.MessageEmbed()
+			.setTitle(title)
+            .addField("Dealer's Hand:", dealerContent, false)
+            .addField(`${message.author.tag}'s Hand:`, displayHand(playerHand), false)
+            .setDescription(playerContent)
+			.setTimestamp()
+
+        let msg = await message.channel.send(embed);
+
+        // add the reactions
         for (let i = 0; i < reactionList.length; i++) {
             msg.react(reactionList[i]);
         }
@@ -27,16 +58,55 @@ module.exports = {
             return reactionList.includes(reaction.emoji.name) && user.id === message.author.id;
         };
         
-        const collector = msg.createReactionCollector(filter, { time: 15000 });
+        const collector = msg.createReactionCollector(filter, { time: 300000 });
         
         collector.on('collect', (reaction, user) => {
             console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
 
             if (reaction.emoji.name === '⬆') {
-                msg.edit('Hit');
+                console.log('Hit');
+                playerHand.push(deck.draw());
+                playerContent = 'The total is ' + sumHand(playerHand);
+
+                // Player win conditions
+                if (sumHand(playerHand) > 21) {
+                    playerContent = 'You Lose'
+                    collector.stop();
+                } else if (sumHand(playerHand) === 21) {
+                    playerContent = 'You Win!';
+                    collector.stop();
+                }
             } else if (reaction.emoji.name === '⏹') {
-                msg.edit('Stay');
+                console.log('Stay');
+                // Dealer's turn and end condition
+                while (sumHand(dealerHand) < 17) {
+                    dealerHand.push(deck.draw());
+                    dealerContent = displayHand(dealerHand);
+                }
+
+                console.log('Dealers hand is ' +  sumHand(dealerHand));
+
+                // Dealer conditions
+                if (sumHand(dealerHand) > 21 || sumHand(playerHand) > sumHand(dealerHand)) {
+                    playerContent = 'You Win'
+                    collector.stop();
+                } else if (sumHand(dealerHand) === 21 || sumHand(playerHand) < sumHand(dealerHand)) {
+                    playerContent = 'You Lose';
+                    collector.stop();
+                } else {
+                    playerContent = 'A DRAW!';
+                    collector.stop();
+                }
             }
+
+            embed = new Discord.MessageEmbed()
+                .setTitle(title)
+                .addField("Dealer's Hand:", dealerContent, false)
+                .addField(`${message.author.tag}'s Hand:`, displayHand(playerHand), false)
+                .setDescription(playerContent)
+                .setTimestamp()
+
+            msg.edit(embed);
 
             removeReactions(msg, message);
         });
@@ -57,4 +127,30 @@ function removeReactions(msg, original) {
     } catch (error) {
         console.error('Failed to remove reactions');
     }
+}
+
+function displayHand(hand) {
+    let output = '';
+
+    for (let i = 0; i < hand.length; i++) {
+        output += hand[i].num + ' of ' + hand[i].suite + '\n';
+    }
+    return output;
+}
+
+function sumHand(hand) {
+    let total = 0;
+    // TODO: for now assume ace is 11
+    // let aceCounter = 0;
+    for (let i = 0; i < hand.length; i++) {
+        if (['Jack', 'Queen', 'King'].includes(hand[i].num)) {
+            total  += 10;
+        } else if (hand[i].num === 'Ace') {
+            total += 11;
+        } else {
+            total += parseInt(hand[i].num);
+        }
+    }
+
+    return total;
 }
