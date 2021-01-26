0a3eb6bcd92b65c010c1cfde14f807dafaab1642
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-01-25 23:35:02 -0500

    test commands cleanup

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'List the server info.',
-	execute(message) {
-		message.channel.send(`**Server name:** ${message.guild.name}\n**Total members:** ${message.guild.memberCount}\n**Owner: **${message.guild.owner}\n**Region: **${message.guild.region}`);
-	},
-};
